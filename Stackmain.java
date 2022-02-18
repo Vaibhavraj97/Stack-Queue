8d74964d1d7b461b8a139ae39ba64b4b6fc03a40
vaibhav rajuskar <vaibhavrajuskar54@gmail.com>
2022-02-18 07:58:53 +0530

    Added main method to pop stack

---
+++ Stackmain.java
@@ -0,0 +1,26 @@
+package com.bridgelabz.StackandQueue;
+
+public class Stackmain {
+	
+	
+
+	    public static void main(String[] args)
+	    {
+	        Stack stack = new Stack();
+	        stack.push(70);
+	        stack.push(30);
+	        stack.push(56);
+	        stack.display();
+
+	        //stack.peek();
+	        stack.pop();
+	        stack.pop();
+	        stack.pop();
+	        stack.pop();
+	        stack.display();
+
+	    }
+
+
+	}
+
