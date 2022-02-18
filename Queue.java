568d6d581ae81e963f3dcb743bffb0012ff027e3
vaibhav rajuskar <vaibhavrajuskar54@gmail.com>
2022-02-18 07:56:09 +0530

    Added method to add no in queue

---
+++ Queue.java
@@ -0,0 +1,33 @@
+package com.bridgelabz.StackandQueue;
+
+	import java.util.LinkedList;
+
+	public class Queue
+	{
+
+	    LinkedList<Integer> queue = new LinkedList<>();
+
+	    public void enqueue(int value)
+	    {
+	        queue.addLast(value);
+	    }
+
+	    public void dequeue()
+	    {
+	        if (queue.size() < 1)
+	            System.out.println(" Queue is empty!");
+	        else
+	            queue.remove();
+	    }
+
+	    public void displayQueue()
+	    {
+	        for (Integer integer : queue)
+	        {
+	            System.out.print(" " + integer);
+	        }
+	        System.out.println();
+	    }
+
+	}
+
